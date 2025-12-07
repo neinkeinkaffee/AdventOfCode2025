e823c3d6d4853528da0f3eeb2d78353ee565e2b6
Gesa Stupperich <gesa@tailscale.com>
2025-12-07 10:14:36 +0000

    Refactor day 3 part 1 to left/right most aware max and add failing test for part 2

--- puzzle_03/src/main.rs
+++ puzzle_03/src/main.rs
@@ -13,27 +13,41 @@ fn main() {
     println!("Total joltage: {sum}")
 }
 
-fn max_joltage(battery: String) -> u32 {
-    let joltages: Vec<u32> = battery.chars()
-        .map(|c| { c.to_digit(10).unwrap() })
+fn max_joltage(battery: String) -> u128 {
+    let joltages: Vec<u128> = battery.chars()
+        .map(|c| { u128::from(c.to_digit(10).unwrap()) })
         .collect();
-    let (m1_index, m1) = max(&joltages);
+    let (m1_index, m1) = left_most_max(&joltages);
 
     if m1_index == joltages.len()-1 {
-        let (_, m0) = max(&joltages[0..m1_index]);
+        let (_, m0) = left_most_max(&joltages[0..m1_index]);
         return m0 * 10 + m1
     }
 
-    let (_, m2) = max(&joltages[m1_index+1..]);
+    let (_, m2) = right_most_max(&joltages[m1_index+1..]);
     m1 * 10 + m2
 }
 
-fn max(elements: &[u32]) -> (usize, u32) {
+fn left_most_max(elements: &[u128]) -> (usize, u128) {
     let mut max = elements[0];
     let mut max_index = 0;
 
     for (i, x) in elements.iter().enumerate() {
         if max < *x  {
+            max = *x;
+            max_index = i;
+        }
+    }
+
+    return (max_index, max);
+}
+
+fn right_most_max(elements: &[u128]) -> (usize, u128) {
+    let mut max = elements[0];
+    let mut max_index = 0;
+
+    for (i, x) in elements.iter().enumerate() {
+        if max <= *x  {
             max = *x;
             max_index = i;
         }
@@ -48,7 +62,7 @@ mod tests {
 
     #[test]
     fn test_max() {
-        assert_eq!(max(&vec!{1, 2, 3, 4}), (3, 4))
+        assert_eq!(right_most_max(&vec!{1, 2, 3, 4}), (3, 4))
 
     }
 
@@ -58,5 +72,13 @@ mod tests {
         assert_eq!(max_joltage("811111111111119".to_string()), 89);
         assert_eq!(max_joltage("234234234234278".to_string()), 78);
         assert_eq!(max_joltage("818181911112111".to_string()), 92);
+    }
+
+    #[test]
+    fn test_max_joltage_12() {
+        assert_eq!(max_joltage("987654321111111".to_string()), 987654321111);
+        assert_eq!(max_joltage("811111111111119".to_string()), 811111111119);
+        assert_eq!(max_joltage("234234234234278".to_string()), 434234234278);
+        assert_eq!(max_joltage("818181911112111".to_string()), 888911112111);
     }
 }
