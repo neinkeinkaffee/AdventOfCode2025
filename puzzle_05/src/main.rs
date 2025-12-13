0b94f2f5e15b4c43c14e9c6bb2e45d570756a93b
Gesa Stupperich <Gesa Stupperich gesa.stupperich@gmail.com>
2025-12-13 09:26:17 +0000

    Day 5 part 2

--- puzzle_05/src/main.rs
+++ puzzle_05/src/main.rs
@@ -6,9 +6,11 @@ fn main() {
     let input: String = read_to_string("input.txt").unwrap();
 
     let (ranges, ids) = parse_input(input);
-    let fresh_ids = count_fresh(ranges, ids);
+    let fresh_ids = count_stocked_ids_in_range(ranges.clone(), ids);
+    let total_ids = count_total_ids_in_range(ranges);
 
-    println!("Fresh ingredients: {fresh_ids}");
+    println!("Fresh ingredients in stock: {fresh_ids}");
+    println!("Total number of fresh ingredients: {total_ids}");
 }
 
 fn parse_input(input: String) -> (Vec<(u128, u128)>, Vec<u128>) {
@@ -47,7 +49,7 @@ fn parse_input(input: String) -> (Vec<(u128, u128)>, Vec<u128>) {
     (ranges, ids)
 }
 
-fn is_fresh(fresh_ranges: &Vec<(u128, u128)>, id: u128) -> bool {
+fn in_range(fresh_ranges: &Vec<(u128, u128)>, id: u128) -> bool {
     for (start, end) in fresh_ranges {
         if *start <= id && id <= *end {
             return true
@@ -56,14 +58,58 @@ fn is_fresh(fresh_ranges: &Vec<(u128, u128)>, id: u128) -> bool {
     false
 }
 
-fn count_fresh(fresh_ranges: Vec<(u128, u128)>, ids: Vec<u128>) -> u128 {
+fn count_stocked_ids_in_range(ranges: Vec<(u128, u128)>, filter_ids: Vec<u128>) -> u128 {
     let mut total = 0;
-    for id in ids {
-        if is_fresh(&fresh_ranges, id) {
+
+    for id in filter_ids {
+        if in_range(&ranges, id) {
             total += 1;
         }
     }
+
     total
+}
+
+fn count_total_ids_in_range(fresh_ranges: Vec<(u128, u128)>) -> u128 {
+    let merged_ranges = merge_ranges(fresh_ranges);
+
+    let mut total = 0;
+    for (start, end) in merged_ranges {
+        let inc = (end - start) + 1;
+        total += inc;
+    }
+
+    total
+}
+
+fn merge_ranges(ranges: Vec<(u128, u128)>) -> Vec<(u128, u128)> {
+    let mut merged_ranges = vec!{};
+
+    let mut sorted_ranges = ranges.clone();
+    sorted_ranges.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let mut ranges_iter = ranges.iter();
+    if let Some(next) = ranges_iter.next() {
+        merged_ranges.push(*next)
+    } else {
+        return merged_ranges;
+    }
+
+    while let Some(next) = ranges_iter.next() {
+        let this = merged_ranges.pop().unwrap();
+        if next.0 <= this.1 {
+            if this.1 <= next.1 {
+                merged_ranges.push((this.0, next.1))
+            } else {
+                merged_ranges.push((this.0, this.1))
+            }
+        } else {
+            merged_ranges.push(this);
+            merged_ranges.push(*next)
+        }
+    }
+
+    merged_ranges
 }
 
 #[cfg(test)]
@@ -89,17 +135,32 @@ mod tests {
     }
 
     #[test]
-    fn test_is_fresh() {
-        assert_eq!(is_fresh(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 1), false);
-        assert_eq!(is_fresh(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 5), true);
-        assert_eq!(is_fresh(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 8), false);
-        assert_eq!(is_fresh(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 11), true);
-        assert_eq!(is_fresh(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 17), true);
-        assert_eq!(is_fresh(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 32), false);
+    fn test_in_range() {
+        assert_eq!(in_range(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 1), false);
+        assert_eq!(in_range(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 5), true);
+        assert_eq!(in_range(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 8), false);
+        assert_eq!(in_range(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 11), true);
+        assert_eq!(in_range(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 17), true);
+        assert_eq!(in_range(&vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, 32), false);
     }
 
     #[test]
-    fn test_count_fresh() {
-        assert_eq!(count_fresh(vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, vec!{1, 5, 8, 11, 17, 32}), 3)
+    fn test_count_in_range_filtered() {
+        assert_eq!(count_stocked_ids_in_range(vec!{(3, 5), (10, 14), (16, 20), (12, 18)}, vec!{1, 5, 8, 11, 17, 32}), 3)
+    }
+
+    #[test]
+    fn test_count_in_range() {
+        assert_eq!(count_total_ids_in_range(vec!{(3, 5)}), 3);
+        assert_eq!(count_total_ids_in_range(vec!{(3, 5), (10, 14)}), 8);
+        assert_eq!(count_total_ids_in_range(vec!{(3, 5), (10, 14), (16, 20)}), 13);
+        assert_eq!(count_total_ids_in_range(vec!{(3, 5), (10, 14), (12, 18), (16, 20)}), 14);
+    }
+
+    #[test]
+    fn test_merge_ranges() {
+        assert_eq!(merge_ranges(vec!{(3, 5), (10, 14)}), vec!{(3, 5), (10, 14)});
+        assert_eq!(merge_ranges(vec!{(3, 5), (10, 14), (12, 18), (16, 20)}), vec!{(3, 5), (10, 20)});
+        assert_eq!(merge_ranges(vec!{(3, 5), (10, 14), (12, 13), (16, 20)}), vec!{(3, 5), (10, 14), (16, 20)});
     }
 }
