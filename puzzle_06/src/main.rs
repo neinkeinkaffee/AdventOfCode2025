2967b8d6cdbbe1b954fff1ac34fe9513f19f1141
Gesa Stupperich <Gesa Stupperich gesa.stupperich@gmail.com>
2025-12-14 11:23:24 +0000

    Day 6 part 2

--- puzzle_06/src/main.rs
+++ puzzle_06/src/main.rs
@@ -9,36 +9,9 @@ fn main() {
     println!("Result: {result}")
 }
 
-fn parse_input(input: &str) -> Vec<Vec<&str>> {
-    let mut result: Vec<Vec<&str>> = vec!{};
-
-    let mut rows: Vec<Vec<&str>> = vec!{};
-    for line in input.split("\n") {
-        if !line.is_empty() {
-            rows.push(line.split_whitespace().collect());
-        }
-    }
-
-    let n_problems;
-    if let Some(first_row) = rows.first() {
-        n_problems = first_row.len()
-    } else {
-        return result
-    }
-
-    for i in 0..n_problems {
-        let problem = rows.iter().map(|row| {
-            return row[i]
-        }).collect();
-        result.push(problem);
-    }
-
-    result
-}
-
-fn solve_problem(problem: &[&str]) -> u128 {
+fn solve_problem(problem: &Vec<String>) -> u128 {
     let n = problem.len();
-    let operator = problem[n-1];
+    let operator = &problem[n-1];
     let numbers = problem[..n-1].iter()
         .filter_map(|s| s.parse::<u128>().ok());
     if operator == "*" {
@@ -53,42 +26,70 @@ fn solve_puzzle(input: &str) -> u128 {
     problems.iter().map(|p| solve_problem(p)).sum()
 }
 
+fn parse_input(input: &str) -> Vec<Vec<String>> {
+    let mut problems: Vec<Vec<String>> = vec![];
+
+    let lines: Vec<_> = input.split("\n")
+        .filter(|l| !l.is_empty())
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .collect();
+    let n_lines = lines.len();
+    if n_lines == 0 {
+        return problems
+    }
+    let n_chars = lines[0].len();
+
+    let mut problem: Vec<String> = vec![];
+    for j in (0..n_chars).rev() {
+        let mut number: String = String::from("");
+        for i in 0..n_lines-1 {
+            let c = &lines[i][j];
+            number.push_str(&c.to_string())
+        }
+        let number_trimmed = number.trim().to_string();
+        if number_trimmed != "" {
+            problem.push(number_trimmed);
+        }
+        let maybe_operator = lines[n_lines-1][j];
+        if maybe_operator != ' ' {
+            problem.push(maybe_operator.to_string());
+            problems.push(problem);
+            problem = vec![];
+        }
+    }
+
+    problems
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_parse_input() {
-        let input = "123 328  51 64  1\n\
-                            45 64  387 23  2\n\
-                             6 98  215 314 3\n\
-                           *   +   *   +   *";
+        let input = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  \n";
 
         assert_eq!(parse_input(input), [
-            ["123", "45", "6", "*"],
-            ["328", "64", "98", "+"],
-            ["51", "387", "215", "*"],
-            ["64", "23", "314", "+"],
-            ["1", "2", "3", "*"],
-        ]);
+            ["4", "431", "623", "+"],
+            ["175", "581", "32", "*"],
+            ["8", "248", "369", "+"],
+            ["356", "24", "1", "*"],
+        ])
     }
 
     #[test]
     fn test_solve_problem() {
-        assert_eq!(solve_problem(&["123", "45", "6", "*"]), 33210);
-        assert_eq!(solve_problem(&["328", "64", "98", "+"]), 490);
-        assert_eq!(solve_problem(&["51", "387", "215", "*"]), 4243455);
-        assert_eq!(solve_problem(&["64", "23", "314", "+"]), 401);
-        assert_eq!(solve_problem(&["1", "2", "3", "*"]), 6);
+        assert_eq!(solve_problem(&["123", "45", "6", "*"].iter().map(|x| x.to_string()).collect::<Vec<String>>()), 33210);
+        assert_eq!(solve_problem(&["328", "64", "98", "+"].iter().map(|x| x.to_string()).collect::<Vec<String>>()), 490);
+        assert_eq!(solve_problem(&["51", "387", "215", "*"].iter().map(|x| x.to_string()).collect::<Vec<String>>()), 4243455);
+        assert_eq!(solve_problem(&["64", "23", "314", "+"].iter().map(|x| x.to_string()).collect::<Vec<String>>()), 401);
+        assert_eq!(solve_problem(&["1", "2", "3", "*"].iter().map(|x| x.to_string()).collect::<Vec<String>>()), 6);
     }
 
     #[test]
     fn test_solve_puzzle() {
-        let input = "123 328  51 64  1\n\
-                            45 64  387 23  2\n\
-                             6 98  215 314 3\n\
-                           *   +   *   +   *";
+        let input = "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  \n";
 
-        assert_eq!(solve_puzzle(input), 4277562)
+        assert_eq!(solve_puzzle(input), 3263827)
     }
 }
